5a0b87ad697463167a71ea605e0f2a8447f89303
Paulo <pmarquesbaronefilho18@protonmail.com>
2024-12-07 14:19:54 -0300

    add: mobile home page

---
+++ assets/script/tailwindConfig.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        branding: '#ED145B',
+        lightGray: '#C2C2C2',
+        gray: '#6D6D6D',
+        darkGray: '#161616',
+        green: '#0DFF00',
+        red: '#C00000',
+      }
+    }
+  }
+}
